bda721a82eaa9fc901ec23c74227e49f19244cae
Karunamay Murmu <karunamaymurmu.km@gmail.com>
2020-08-10 12:31:21 +0530

    Bundling

--- buildScript/srcServer.js
+++ buildScript/srcServer.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 let port = 8000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'))
 })
 
-app.listen(port, function (error) { error ? console.log(error) : open(`http://localhost:${port}`) })
+app.listen(port, function (error) { return error ? console.log(error) : open(`http://localhost:${port}`) })
